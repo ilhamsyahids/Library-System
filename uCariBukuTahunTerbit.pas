c2d1b94d62adb2ced43609bbf1d53ca74fbc50a5
ilhamsyahids <ilhamsyahids@gmail.com>
2019-04-18 06:49:54 +0700

    Update uCariBukuTahunTerbit.pas
    uCariBukuTahunTerbit
    - Bug fixed

--- uCariBukuTahunTerbit.pas
+++ uCariBukuTahunTerbit.pas
@@ -5,53 +5,29 @@ unit uCariBukuTahunTerbit;
 
 // Kamus
 interface
-    uses uTipeData;
+    uses uTipeData, uCari;
     procedure CariTahunTerbit(array_Buku : Arr_Buku);
-    procedure sort_string(array_buku : Arr_Buku);
 
 // Algoritma
 implementation
-    // Procedure sort array of string
-    procedure sort_string(array_buku : Arr_Buku);
-        // variabel array_buku diisi dengan daftar judul buku
-        // pada procedure Cari
-        var
-            i, j, n : Integer;
-            temp : Buku;
-
-        begin
-            n := Length(array_buku);
-            for i:=0 to n-1 do begin
-                for j:=0 to n-1 do begin
-                    if (array_buku[j].judul_buku >= array_buku[j+1].judul_buku) then
-                        begin
-                            temp          := array_buku[j+1];
-                            array_buku[j+1] := array_buku[j];
-                            array_buku[j] := temp;
-                    end;
-                end;
-            end;
-        end;
-
-    
     // Procedure cari buku berdasarkan kategori 
-    // menggunakan procedure sort array of string
+    // menggunakan procedure sort array of string dari unit uCari
     procedure CariTahunTerbit(array_Buku : Arr_Buku);
         // variabel array_buku diisi dengan daftar buku
         // pada program utama
         var
         	tahun : integer;
             jenis : string;
-            i, j, n : Integer;
+            i, j, n, nk : Integer;
             ada, valid : Boolean;
-            tahunbuku : Arr_buku;
+            tahunbuku : Arr_String;
 
         begin
             valid := False;
             ada := False;
             i := 0;
             n := Length(array_Buku);
-            SetLength(tahunbuku, 0);
+            
             
             // Skema validasi untuk input kategori
             repeat
@@ -60,10 +36,12 @@ implementation
                 case jenis of
                     '<' : 
                         begin 
+                            nk := 0;
             				while i<n do begin
                 				if (array_Buku[i].Tahun_Penerbit < tahun) then begin
-                    				SetLength(tahunbuku, Length(tahunbuku)+1);
-    								tahunbuku[High(tahunbuku)] := array_buku[i];
+                    				nk := nk + 1;
+                                    SetLength(tahunbuku, nk);
+    								tahunbuku[nk-1] := array_buku[i].Judul_Buku;
     								ada := true;
                 				end;
                 				i := i + 1;
@@ -72,10 +50,12 @@ implementation
                         end;
                     '<=' : 
                         begin 
+                            nk := 0;
             				while i<n do begin
                 				if (array_Buku[i].Tahun_Penerbit <= tahun) then begin
-                    				SetLength(tahunbuku, Length(tahunbuku)+1);
-    								tahunbuku[High(tahunbuku)] := array_buku[i];
+                    				nk := nk+1;
+                                    SetLength(tahunbuku, nk);
+    								tahunbuku[nk-1] := array_buku[i].Judul_Buku;
     								ada := true;
                 				end;
                 				i := i + 1;
@@ -84,10 +64,12 @@ implementation
                         end;
                     '=' : 
                         begin 
+                            nk := 0;
             				while i<n do begin
                 				if (array_Buku[i].Tahun_Penerbit = tahun) then begin
-                    				SetLength(tahunbuku, Length(tahunbuku)+1);
-    								tahunbuku[High(tahunbuku)] := array_buku[i];
+                    				nk := nk+1;
+                                    SetLength(tahunbuku, nk);
+    								tahunbuku[nk-1] := array_buku[i].Judul_Buku;
     								ada := true;
                 				end;
                 				i := i + 1;
@@ -96,10 +78,12 @@ implementation
                         end;
                     '>=' : 
                         begin 
+                            nk := 0;
             				while i<n do begin
                 				if (array_Buku[i].Tahun_Penerbit >= tahun) then begin
-                    				SetLength(tahunbuku, Length(tahunbuku)+1);
-    								tahunbuku[High(tahunbuku)] := array_buku[i];
+                    				nk := nk+1;
+                                    SetLength(tahunbuku, nk);
+    								tahunbuku[nk-1] := array_buku[i].Judul_Buku;
     								ada := true;
                 				end;
                 				i := i + 1;
@@ -108,10 +92,12 @@ implementation
                         end;
                     '>' : 
                         begin 
+                            nk := 0;
             				while i<n do begin
                 				if (array_Buku[i].Tahun_Penerbit > tahun) then begin
-                    				SetLength(tahunbuku, Length(tahunbuku)+1);
-    								tahunbuku[High(tahunbuku)] := array_buku[i];
+                    				nk := nk+1;
+                                    SetLength(tahunbuku, nk);
+    								tahunbuku[nk-1] := array_buku[i].Judul_Buku;
     								ada := true;
                 				end;
                 				i := i + 1;
@@ -130,13 +116,16 @@ implementation
             Writeln();
             WriteLn('Buku yang terbit ', jenis, ' ', tahun);
             sort_string(tahunbuku);
-            for j:=0 to (Length(tahunbuku)-1) do
-            begin
-              writeln(tahunbuku[j].ID_Buku, ' | ', tahunbuku[j].Judul_Buku, ' | ', tahunbuku[j].Author);
+            for i:=0 to nk do begin
+                for j:=0 to n-1 do begin
+                    if (tahunbuku[i] = array_Buku[j].Judul_Buku) then begin
+                        writeln(array_Buku[j].ID_Buku, ' | ', array_Buku[j].Judul_Buku, ' | ', array_Buku[j].Author);
+                    end;
+                end;
             end;
-            if not(ada) then
-            begin
-            WriteLn('Tidak ada buku dalam kategori ini.');
+            
+            if not(ada) then begin
+                WriteLn('Tidak ada buku dalam kategori ini.');
             end;
         end;
 end.
